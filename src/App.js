97056eabaf055fb970a2372448461516e700b2ac
Devbeekind <mykyta.kolysnychenko@nure.ua>
2022-07-07 19:06:30 +0200

    0.1b
    under_nav_fc WIP

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header-fixed">
+        <nav className='navbar'> 
+        <table>
+          <tr>
+          <td>
+            <div className='leftside_navbar'>
+            <div id='Button_left№1'><button>Button</button></div>
+            <div id='Button_left№2'><button>Button</button></div>
+            </div>
+        </td>
+        <td> 
+        <div className='rightside_navbar'>
+          <div id='Button_right№1'><button>Button</button></div>
+          <div id='Button_right№2'><button>Button</button></div>
+        </div>
+        </td>
+        <div className='lang_button'>
+          <div id='Button_lang'> <button>lang</button></div>
+        </div>
+        </tr>
+        </table>
+        </nav>
       </header>
     </div>
   );
---
+++ src/under_nav.js
@@ -0,0 +1,11 @@
+import './under_nav.css';
+
+function under_nav_fc () {
+    return (
+        <div className='under_nav_box'>
+            1
+        </div>
+    );
+}
+
+export default under_nav_fc;
